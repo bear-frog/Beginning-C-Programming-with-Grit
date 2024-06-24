3c277dfe9f76bfd3544112e93bac053109055baa
egnachio <egnachio@wins21.co.kr>
2024-06-24 13:40:51 +0900

    1

---
+++ Section_9/scanfAge/scanfAge/scanfAge.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	char szName[32] = { 0 };
+	int nAge = 0;
+
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d%*c", &nAge);
+
+	printf("이름을 입력하세요. : ");
+	gets_s(szName, sizeof(szName));
+
+	printf("%d, %s\n", nAge, szName);
+}
